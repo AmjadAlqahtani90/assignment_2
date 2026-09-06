cea96f2949b5588a8bf81ba419cdc7bda8b4bf49
Amjad Alqahtani <94429151+AmjadAlqahtani90@users.noreply.github.com>
2026-09-06 18:49:31 +0200

    Update task.cpp
    Memory Management C++

--- task.cpp
+++ task.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+
+    int* numbers = new int[5];
+
+    for (int i = 0; i < 5; i++) {
+        numbers[i] = (i + 1) * 10;
+    }
+
+    cout << "Numbers:" << endl;
+
+    for (int i = 0; i < 5; i++) {
+        cout << numbers[i] << endl;
+    }
+
+    delete[] numbers;
+    numbers = nullptr;
+
+    return 0;
+}
